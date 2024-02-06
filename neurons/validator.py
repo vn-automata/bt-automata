fd46a26e1152dd6b3700c32d2244bdec70b6d8a7
Wayne Small <waynemystir@gmail.com>
2024-02-05 18:41:39 -0600

    add validator forward, scoring, and reward

--- neurons/validator.py
+++ neurons/validator.py
@@ -30,14 +30,21 @@ from template.validator import forward
 # import base validator class which takes care of most of the boilerplate
 from template.base.validator import BaseValidatorNeuron
 
+#Internal modules
+from template.utils import rulesets
+from template.utils.rulesets import rule_classes
+
 
 class Validator(BaseValidatorNeuron):
     """
-    Your validator neuron class. You should use this class to define your validator's behavior. In particular, you should replace the forward function with your own logic.
+    Validator class for cellular automata protocol. This class will generate a random cellular automata rule and initial state, 
+    and then query the miners for their responses. It will then reward the miners based on the quality of their responses.
+    1-diimensional cellular automata are supported through the cpl library. The rule is chosen from a set of class 3/4 rules, 
+    and the initial state is a random 1D numpy array, such that the store of all agent states will be a 2-D numpy array indexed on time (steps).
 
-    This class inherits from the BaseValidatorNeuron class, which in turn inherits from BaseNeuron. The BaseNeuron class takes care of routine tasks such as setting up wallet, subtensor, metagraph, logging directory, parsing config, etc. You can override any of the methods in BaseNeuron if you need to customize the behavior.
+    Default: This class inherits from the BaseValidatorNeuron class, which in turn inherits from BaseNeuron. The BaseNeuron class takes care of routine tasks such as setting up wallet, subtensor, metagraph, logging directory, parsing config, etc. You can override any of the methods in BaseNeuron if you need to customize the behavior.
 
-    This class provides reasonable default behavior for a validator such as keeping a moving average of the scores of the miners and using them to set weights at the end of each epoch. Additionally, the scores are reset for new hotkeys at the end of each epoch.
+    Default: This class provides reasonable default behavior for a validator such as keeping a moving average of the scores of the miners and using them to set weights at the end of each epoch. Additionally, the scores are reset for new hotkeys at the end of each epoch.
     """
 
     def __init__(self, config=None):
@@ -46,19 +53,85 @@ class Validator(BaseValidatorNeuron):
         bt.logging.info("load_state()")
         self.load_state()
 
-        # TODO(developer): Anything specific to your use case you can do here
+    def get_random_params(self):
+        # Generate a random initial state as a 1D numpy array from cpl
+
+        # size defines the dimensionn of the 1-D array. Between 100-1000
+        size = random.randint(100, 1000)
+
+        # Generate the initial state using the ruelsets module
+        initial_state = rulesets.InitialConditions(size)
+
+        # Choose a random number of time-steps, between 100 and 1000
+        steps = random.randint(1000, 5000)
+
+        # Choose a random rule function. Limit to Class 3/4 rules in 1D. Covert it to a rule function using the rule_classes dictionary.
+        rule_name = random.choice(["Rule30", "Rule54", "Rule62", "Rule110", "Rule124", "Rule126"])
+        rule_func = rule_classes.get(rule_name)
+
+        if rule_func is not None:
+        # Rule name found, proceed!
+        else:
+            # Rule name not found. Sound the alarm
+            raise ValueError(f"Rule '{rule_name}' not found in rule_classes dictionary.")
+
+        # Log and return the parameters.
+        if initial_state is not None and steps is not None and rule_func is not None:
+            bt.logging.info(
+                f"Generated cellular automata parameters: {initial_state}, {steps}, {rule_name}"
+            )
+        return initial_state, steps, rule_func
+
 
     async def forward(self):
         """
         Validator forward pass. Consists of:
         - Generating the query
+        initial_state, steps, rule_func = self.get_random_params()
+        - Running the simulation
+        rulesets.Simulate1D(initial_state, steps, rule_func, r=1).run()
         - Querying the miners
         - Getting the responses
         - Rewarding the miners
         - Updating the scores
         """
-        # TODO(developer): Rewrite this function based on your protocol definition.
-        return await forward(self)
+
+        sample_size = min(self.config.neuron.sample_size, self.metagraph.n.item())
+        miner_uids = template.utils.uids.get_random_uids(
+            self,
+            k=sample_size,
+        )
+
+        initial_state, timesteps, rule_func = self.get_random_params()
+        synapse = template.protocol.CAsynapse(
+            initial_state=initial_state,
+            timesteps=timesteps,
+            rule_func=rule_func,
+        )
+
+        # The dendrite client queries the network.
+        responses = self.dendrite.query(
+            # Send the query to selected miner axons in the network.
+            axons=[self.metagraph.axons[uid] for uid in miner_uids],
+            # Pass the synapse to the miner.
+            synapse=synapse,
+            # Do not deserialize the response so that we have access to the raw response.
+            deserialize=False,
+        )
+
+        # Log the results for monitoring purposes.
+        bt.logging.info(f"Received responses: {responses}")
+
+        rewards = template.validator.reward.get_rewards(
+            self,
+            query_synapse=synapse,
+            responses=responses,
+        )
+
+        bt.logging.info(f"Scored responses: {rewards}")
+
+        # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
+        self.update_scores(rewards, miner_uids)
 
 
 # The main function parses the configuration and runs the validator.
--- template/validator/reward.py
+++ template/validator/reward.py
@@ -18,10 +18,19 @@
 # DEALINGS IN THE SOFTWARE.
 
 import torch
-from typing import List
+from typing import Any, List
+
+import numpy as np
+from numpy.typing import NDArray
+
+from template.protocol import CAsynapse
+from template.utils.rulesets import rulesets
 
 
-def reward(query: int, response: int) -> float:
+def reward(
+    ground_truth_array: NDArray[Any],
+    response: CAsynapse,
+) -> float:
     """
     Reward the miner response to the dummy request. This method returns a reward
     value for the miner, which is used to update the miner's score.
@@ -30,13 +39,15 @@ def reward(query: int, response: int) -> float:
     - float: The reward value for the miner.
     """
 
-    return 1.0 if response == query * 2 else 0
+    pred_array = response.array_data
+    are_eq = np.array_equal(ground_truth_array, pred_array)
+    return 1.0 if are_eq else 0.0
 
 
 def get_rewards(
     self,
-    query: int,
-    responses: List[float],
+    query_synapse: CAsynapse,
+    responses: List[CAsynapse],
 ) -> torch.FloatTensor:
     """
     Returns a tensor of rewards for the given query and responses.
@@ -48,7 +59,12 @@ def get_rewards(
     Returns:
     - torch.FloatTensor: A tensor of rewards for the given query and responses.
     """
+
+    initial_state = query_synapse.initial_state
+    timesteps = query_synapse.timesteps
+    rule_func = query_synapse.rule_func
+    gt_array = rulesets.Simulate1D(initial_state, timesteps, rule_func, r=1).run()
     # Get all the reward results by iteratively calling your reward() function.
     return torch.FloatTensor(
-        [reward(query, response) for response in responses]
+        [reward(gt_array, response) for response in responses]
     ).to(self.device)
